c532531753b3e34d8940d06851816cbfda98a5c8
xieyu567 <xieyu345800@sina.com>
2022-11-03 10:10:14 +0800

    add sqlx migrate

---
+++ migrations/20221102011125_init.down.sql
@@ -0,0 +1,3 @@
+-- Add down migration script here
+DROP SCHEMA rsvp CASCADE;
+DROP EXTENSION btree_gist;
---
+++ migrations/20221103005430_reservation.down.sql
@@ -0,0 +1,4 @@
+-- Add down migration script here
+DROP TABLE rsvp.reservations CASCADE;
+DROP TYPE rsvp.reservation_status;
+DROP TYPE rsvp.reservation_update_type;
